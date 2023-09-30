79e78edd81558f46377ffc140ddf10abca3892a5
MUNEEBA MEHMOOD <124117980+engrmuneebamehmood@users.noreply.github.com>
2023-09-30 19:34:10 +0500

    Add files via upload

--- Link.js
+++ Link.js
@@ -1,20 +1,29 @@
 import classNames from 'classnames';
 import useNavigation from '../Hooks/useNavigation';
 
-function Link({ to, children }) {
-  const { navigate } = useNavigation();
-const classes=classNames('text-blue-500')
+function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
+
+  const classes = classNames(
+    'text-blue-500',
+    className,
+    currentPath === to && activeClassName
+  );
+
   const handleClick = (event) => {
-    if (event.metaKey || event.ctrlKey)
-    {
-        return
+    if (event.metaKey || event.ctrlKey) {
+      return;
     }
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a className={classes} href={to} onClick={handleClick}>{children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
